0155652ec68b28e7fcc732450b3a65353b9aa8b8
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-03-14 11:46:23 +0000

    Added binarySearch

--- binarySearch.js
+++ binarySearch.js
@@ -12,5 +12,18 @@ function binarySearch(array, key) {
             right = mid - 1;
         }
     }
-    return -1;
+    return null;
 }
+
+function verify(index) {
+    if (index !== null) {
+    console.log("Target found at index:", index);
+    } else {
+    console.log("Target not found in list");
+    }
+}
+  
+const index = binarySearch([1,2,3,4,5,6,7,8,9,10], 12);
+verify(index);
+
+
